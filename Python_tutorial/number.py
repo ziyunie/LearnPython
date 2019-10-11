4e2d0f40edafea30d0f660094d3e12b982d4b807
Will <283518376@qq.com>
2019-10-11 11:01:16 +0800

    update

---
+++ Python_tutorial/number.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3.7
+# -*- coding:utf-8 # -*- 
+"""
+
+@File    : number.py
+@Author  : WILL NIE
+@Email   : ziyunie@hotmail.com
+@Create_Time :  11:30
+
+"""
+
+# 加法
+add = 3 + 4
+print (f'4 + 3 = {add}')
+
+# sub
+sub =11 - 4
+print(f'11 - 4={sub}')
+
+# multi
+multi = 26 * 3
+print (f'26 * 3 = {multi}')
+
+#div
+div = 62 / 3
+print(f'62 / 3 = {div}')
+
+#delivery
+delivery = 62%3
+print(f'62 % 3 = {delivery}')
+
+# round_num
+round_num = 62 // 3
+print(f'62 // 3 = {round_num}')
+
+#power
+power= 62**3
+print(f'62 ** 3 = {power}')
